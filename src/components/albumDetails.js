41169ca0a42b8b6198329b8beafaa0b803d0c00c
tdimnet <thomas.dimnet@gmail.com>
2017-05-23 11:38:52 +0200

    Create card in order to display the albums details

--- src/components/albumDetails.js
+++ src/components/albumDetails.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {
-  Text, View, Image
+  Text, View, Image, Linking
 } from 'react-native';
 
+import Card from './card';
+import CardSection from './cardSection';
+
 const AlbumDetails = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
+
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
     <View>
       <View>
@@ -16,5 +28,30 @@ const AlbumDetails = ({ album }) => {
     </View>
   );
 }
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 
 export default AlbumDetails;
